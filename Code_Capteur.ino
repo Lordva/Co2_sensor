f4bc3ddcda14ff2ada78fbefcc4d4fe645f6e665
Louis | Lordva <louis.chabanon@gmail.com>
2021-05-30 19:19:31 +0200

    Added Wifi support and http request

--- Code_Capteur.ino
+++ Code_Capteur.ino
@@ -1,13 +1,20 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+
 #define Gaz 4
 
 const char* ssid = "";
 const char* password = "";
 
+String Host = "";
+
+unsigned long lastTime = 0;
+unsigned long sendInterval = 600000;
+
 void setup(){
   Serial.begin(9600);
   pinMode(Gaz, INPUT);
-  WiFi.mode(WIFI_STA);
+  initWifi(ssid, password);
 }
 
 void loop(){
@@ -15,18 +22,61 @@ void loop(){
   int threashold;
   Serial.println(gazsensor);
 
-  //if(gazsensor > 1000){
-    //Serial.println("Alert");
-  //}
+  if(gazsensor > 2000){
+    Serial.println("Alert");
+    SendData(Host, gazsensor);
+  }
+  
   delay(100);
 }
 
-void initWifi(){
-  WiFi.begin(ssid, password);
+void SendData(String _host, int _value){
+
+  if((millis() - lastTime) > sendInterval){
+    if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+
+    String serverPath = _host + "?reading=" + _value;
+
+    http.begin(serverPath.c_str());
+
+    int httpResponceCode = http.GET();
+
+    if(httpResponceCode>0){
+      Serial.print("HTTP Responce code: ");
+      Serial.println(httpResponceCode);
+      String payload = http.getString();
+      Serial.println(httpResponceCode);
+    }
+    else {
+      Serial.print("[Error] Http error code : ");
+      Serial.println(httpResponceCode);
+    }
+    http.end();
+  }
+    else{
+    Serial.println("Wifi disconnected !");
+    }
+  }
+  lastTime = millis();
+}
+
+void initWifi(const char* SSID, const char* pwd){
+  int timer = 10000 + millis();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, pwd);
+
   Serial.print("Connection au Wifi ..");
+
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
+    if(millis() > timer){
+      Serial.println("[ERROR] Timeout...");
+      break;
+      return;
+    }
     delay(1000);
   }
-  Serial.println("Connécter au Wifi avec l'addresse : " + WiFi.localIP())
+  Serial.println("Connecter au Wifi avec l'addresse : " + WiFi.localIP());
 }
